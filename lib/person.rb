37db443a0d3756bc860a29444fb4f329bddb7ee6
Jonathan Block <blockjonathan29@gmail.com>
2021-02-12 02:33:29 -0500

    commit tree 7ac4042397c910ab9293998f3534203cd1a4fcca

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,74 @@
-# your code goes here
+class Person
+    attr_reader :name
+    attr_accessor :happiness, :hygiene, :bank_account
+
+  def initialize(name, bank_account= 25, hygiene= 8, happiness= 8)
+    @name = name 
+    @bank_account = bank_account
+    @hygiene = hygiene
+    @happiness = happiness
+  end
+
+  def hygiene
+    @hygiene.clamp(0, 10)
+  end
+
+  def happiness
+    @happiness.clamp(0, 10)
+  end
+
+  def clean?
+    @hygiene > 7 ? true : false
+  end
+  
+  def happy?
+    @happiness > 7 ? true : false
+  end
+
+  def take_bath
+    self.hygiene += 4
+    return "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+
+
+  def work_out
+    self.hygiene -= 3
+    self.happiness += 2
+    return "♪ another one bites the dust ♫"
+  end
+
+
+
+
+  def get_paid(bank_account)
+    salary = 100
+    self.bank_account += salary
+    return "all about the benjamins"
+end
+
+  def call_friend(friend)
+    self.happiness += 3
+    friend.happiness += 3
+    return "Hi #{friend.name}! It's #{@name}. How are you?"
+  end
+
+
+
+  def start_conversation(friend, topic)
+    if topic == "politics"
+      self.happiness -= 2
+      friend.happiness -= 2
+      return "blah blah partisan blah lobbyist"
+    elsif topic == "weather"
+       self.happiness += 1
+       friend.happiness += 1
+       return "blah blah sun blah rain"
+    else
+        return "blah blah blah blah blah"
+    end
+  end
+
+end
+
+friend = Person.new("Penelope")
+
